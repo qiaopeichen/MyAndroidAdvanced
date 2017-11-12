21bb34e89cce2aa742e29e10b9f7b6ca0f288328
qiaopc <qiaopc@gionee.com>
2017-11-12 15:27:54 +0800

    解析OkHttp

--- 网络编程与网络框架/解析OKHttp/2.解析OKHttp.java
+++ 网络编程与网络框架/解析OKHttp/2.解析OKHttp.java
@@ -346,6 +346,185 @@ boolean cleanupRunning;
 */
 
 //	ConnectionPool的构造方法如下所示：
+public ConnectionPool() {
+	this(5, 5, TimeUnit.MINUTES);
+}
+public ConnectionPool(int maxIdleConnections, long keepAliveDuration, TimeUnit timeUnit) {
+	this.maxIdleConnections = maxIdleConnections;
+	this.keepAliveDurationNs = thisUnit.toNanos(keepliveDuration);
+	if (keepAliveDuration <= 0) {
+		throw new IllegalArgumentException("keepAliveDuration <= 0:" + keepAliveDuration);
+	}
+}
+
+// ConnectionPool是在OkHttpClient实例化时创建的：
+public OkHttpClient() {
+	this (new Builder());
+}
+
+public Builder() {
+	dispatcher = new Dispatcher();
+	...
+	connectionPool = new ConnectionPool();
+	...
+}
+
+/* 
+	缓存操作
+	ConnectionPool提供对Deque<RealConnection>进行操作的方法分别为put、get、connectionBecameIdle和evictAll这几个操作。
+	分别对应放入连接、获取连接、移除连接和移除所有连接操作。
+	这里我们举例说明put和get操作。
+*/
+void put(RealConnection connection) {
+	assert (Thread.holdsLock(this));
+	if (!clernupRunning) {
+		cleanupRunning = true;
+		executor.execute(cleanupRunnable);
+	}
+	connections.add(connection);
+}
+// 在添加到Deque之前首先要清理空闲的线程，这个后面会讲到，再来看get操作：
+RealConnection get(Address address, StreamAllocation streamAllocation) {
+	assert (Thread.holdsLock(this));
+	for (RealConnection connection : connections) {
+		if (connection.allocations.size() < connection.allocationLimit && address.equals(connection.route().address) && !connection.noNewStreams) {
+			streamAllocation.acquire(connection);
+			return connection;
+		}
+	}
+	return null;
+}
+// 遍历connections缓存列表。当某个连接计数的次数小于限制的大小，并且request的地址和缓存列表中此连接的地址完全匹配时，则直接复用缓存列表中的connection作为request的连接。
+
+/*
+	自动回收连接
+	OkHttp是根据StreamAllocation引用计数是否为0来实现自动回收连接的。我们在put操作前首先要调用executor.execute(cleanupRunnable)来清理闲置的线程。
+	查看cleanuRunnable到底做了什么：
+*/
+private final Runnable cleanupRunnbale = new Runnable() {
+	@Override
+	public void run() {
+		while (true) {
+			long waitNanos = cleanup(System.nanoTime());
+			if (waitNanos == -1) return;
+			if (waitNanos > 0) {
+				long waitMillis = waitNanos / 1000000L;
+				waitNanos -= (waitMillis * 1000000L);
+				synchronized (ConnectionPool.this) {
+					try {
+						ConnectionPool.this.wait(waitMillis, (int) waitNanos);
+					}
+				}
+			}
+		}
+	}
+};
+/*
+	线程不断地调用cleanup方法来进行清理，并返回下次需要清理的间隔时间，然后调用wait方法进行等待以释放锁与时间片。
+	当等待时间到了后，再次进行清理，并返回下次要清理的间隔时间。如此循环下去。接下来看看cleanup方法，如下所示：
+*/
+long cleanup(long now) {
+	int inUseConnectionCount = 0;
+	int idleConnectionCount = 0;
+	RealConnection longestIdleConnection = null;
+	long longestIdleDurationNs = Long.MIN_VALUE;
+	synchronized (this) {
+		for (Iterator<RealConnection> i = connectinos.iterator(); i.hasNext; ) {
+			RealConnection connection = i.next();
+			if (pruneAndGetAllocationCount(connection, now) > 0) {// 1
+				inUseConnectionCount++;
+				continue;
+			}
+			idleConnectionCount++;
+			long idleDurationNs = now - connection.idleAtNanos;
+			if (idleDurationNs > longestIdleDurationNs) {
+				longestIdleDurationNs = idleDurationNs;
+				longestIdleConnection = connection;
+			}
+		}
+		if (longestIldeDurationNs >= this.keepAliveDurationNs || idleConnectionCount > this.maxIdleConnections) { // 2
+			connections.remove(longestIdleConnection);
+		} else if (idleConnectionCount > 0) {
+			return keepAliveDurationNs - longestIdleDurationNs;
+		} else if (inUseConnectionCount > 0) {
+			return keepAliveDurationNs;
+		} else {
+			cleanupRunning = false;
+			return -1; // 3
+		}
+	}
+	closeQuietly(longestIdleConection.socket());
+	return 0;
+}
+/*
+	cleanup方法所做的事情简单总结就是，根据连接中的引用计数来计算空闲连接数和活跃连接数，然后标记出空闲的连接。
+	在上面代码注释2处，如果空闲连接keepAlive时间超过5分钟，或者空闲连接数超过5个，则从Deque中移除此连接。
+	接下来根据空闲连接或者活跃连接来返回下次需要清理的时间数：
+	如果空闲连接大于0，则返回此连接即将到期的时间；
+	如果都是活跃连接并且大于0，则返回默认的keepAlive时间5分钟。
+	在注释3处，如果没有任何连接，则跳出循环并返回-1.
+	在注释1处，通过pruneAndGetAllocationCount方法来判断连接是否闲置。如果pruneAndGetAllocationCount方法的返回值大于0则是活跃连接，否则就是空闲连接。
+	接下来查看pruneAndGetAllocationCount方法，如下所示：
+*/
+private int pruneAndGetAllocationCount(RealConnection connection, long now) {
+	List<Reference<StreamAllocation>> references = connection.allocations;
+	for (int i = 0; i < references.size(); ) {
+		Reference<StreamAllocation> reference = references.get(i);
+		if (reference.get() != null) {
+			i++;
+			continue;
+		}
+		Internal.logger.warning("A connection to " + connection.route().address().url() + " was leaked. Did you forget to close a response body?");
+		references,remove(i);
+		connection.noNewStreams = true;
+		if (references.isEmpty()) { // 1
+			connection.idleAtNanos = now - keepAliveDurationNs;
+			return 0;
+		}
+	}
+	return references.size();
+}
+/*
+	pruneAndGetAllocationCount方法首先遍历传进来的RealConnection的StreamAllocation列表。
+	如果StreamAllocation被使用，则接着遍历下一个StreamAllocation;如果StreamAllocation未被使用，则从列表中移除。
+	在上面代码注释1处，如果列表为空，则说明此连接没有引用了，这时返回0，表示此连接是空闲连接；否则就返回非0的数，表示此连接是活跃连接。
+	那么StreamAllocation是什么，怎么才能判断StreamAllocation使用与否？
+*/
+/*
+	在OkHttp的高层代码中，使用了类似于引用计数的方式跟踪socket流的调用。
+	这里的计数对象是StreamAllocation，它被反复执行acquire与release操作，这两个方法其实是在改变RealConnection中的List<Reference<StreamAllocation>>的大小。
+	StreamAllocation的acquire方法和release方法如下所示：
+*/
+public void acquire(RealConnection connection) {
+	connection.allocations.add(new WeakReference<>(this));
+}
+private void release(RealConnection connection) {
+	for (int i = 0; size = connection.allocations.size(); i < size; i++) {
+		Reference<StreamAllocation> reference = connection.allocations.get(i);
+		if (reference.get() == this) {
+			connection.allocations.remove(i);
+			return;
+		}
+	}
+	throw new IllegalStateException();
+}
+/*
+	RealConnection是socket物理连接的包装，它里面维护了List<Reference<StreamAllocation>>的引用。
+	List中StreamAllocation的数量也就是socket被引用的计数。如果计数为0，则说明此连接没有被使用，也就是空闲的，需要通过下文的算法实现回收；
+	如果计数不为0，则表示上层代码仍然在引用，就无需关闭连接。
+*/
+
+/*
+	可以看出连接池复用的核心就是用Deque<RealConnection>来存储连接，通过put,get,connectionBecameIdle和evictAll几个操作来对Deque进行操作，
+	另外通过判断连接中的计数对象StreamAllocation来进行自动回收连接。
+*/
+
+
+
+
+
+
+
 
 
 
